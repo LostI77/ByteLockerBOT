4184b5779e8ce3eec512a3d9740816eb4b09e1f2
Losti! <lostforyou67a@gmail.com>
2024-07-21 14:03:04 -0500

    Updating Static Constants

---
+++ src/constants/commands.py
@@ -0,0 +1,10 @@
+pathCommands = [
+    "functions.commands.admin.bash.index",
+    "functions.commands.admin.logs.index",
+    "functions.commands.admin.mute.index",
+    "functions.commands.admin.nuke.index",
+    "functions.commands.admin.surveys.index",
+    "functions.commands.client.virustotal.index",
+    "functions.commands.client.user.index",
+    "functions.commands.client.music.index"
+]
